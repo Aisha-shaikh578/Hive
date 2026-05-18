8d4181e9ee5e4e5710577c7adcd4536ec0c93574
Aisha-shaikh578 <itsaishashaikh05@gmail.com>
2026-05-18 20:22:30 +0530

    Added Sign Up section at the bottom

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 import PostFeed from "@/components/PostFeed";
 import SideBar from "@/components/SideBar";
+import SignUpPrompt from "@/components/SignUpPrompt";
 import Widgets from "@/components/Widgets";
 
 export default function Home() {
   return (
+    <>
     <div
-     className="text-[#0f1419] min-h-screen max-w-[1400px mx-auto flex"
+     className="text-[#0f1419] min-h-screen max-w-[1400px mx-auto flex 2xl:justify-center"
     >
     <SideBar />
     <PostFeed />
     <Widgets />
     </div>
+
+     <SignUpPrompt />
+    </>
   );
 }
---
+++ components/SignUpPrompt.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function SignUpPrompt() {
+  return (
+    <div className='fixed w-full h-[80px] bg-[#f4af01] bottom-0 flex justify-center lg:justify-between lg:px-20 xl:px-40 2xl:px-80 items-center space-x-5'>
+     <div className='md:flex flex-col text-white hidden'>
+       <span className='text-xl'>
+         Don't miss out the buzz
+       </span>
+       <span>
+        People on Hive are always the first to know
+       </span>
+     </div>
+
+     <div className='flex space-x-2'>
+      <button className='w-[88px] h-[40px] text-sm border-2 border-gray-100 rounded-full text-white font-bold hover:bg-white hover:bg-opacity-25 transition'>
+        Log In
+      </button>
+      <button className='w-[88px] h-[40px] text-sm border-2 border-gray-100 rounded-full bg-white font-bold hover:bg-opacity-45 transition'>
+        Sign Up
+      </button>
+     </div>
+    </div>
+  )
+}
