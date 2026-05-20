457c210119ee55e9a98bd0ae43b9797f3819ac98
Aisha-shaikh578 <itsaishashaikh05@gmail.com>
2026-05-20 23:06:44 +0530

    Fetch and displayed post data on feed from firebase storage

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,11 +1,22 @@
 import { ArrowUpTrayIcon, ChartBarIcon, ChatBubbleOvalLeftEllipsisIcon, HeartIcon } from '@heroicons/react/24/outline'
+import { DocumentData, Timestamp } from 'firebase/firestore';
 import Image from 'next/image'
 import React from 'react'
+import Moment from 'react-moment';
 
-export function Post() {
+interface PostProps{
+  data: DocumentData
+}
+
+export function Post({data}: PostProps) {
   return (
     <div className='border-b border-gray-100'>
-      <PostHeader />
+      <PostHeader 
+      username={data.username}
+      name={data.name}
+      timestamp={data.timestamp}
+      text={data.text}
+      />
 
       <div className='ml-16 p-3 flex space-x-14'>
         <div className='relative'>
@@ -34,8 +45,14 @@ export function Post() {
   )
 }
 
+interface PostHeaderProps {
+  name: string;
+  username: string;
+  timestamp: Timestamp,
+  text: string
+}
 
-export function PostHeader() {
+export function PostHeader({username, name, timestamp, text}: PostHeaderProps) {
   return (
     <div className='flex p-3 space-x-5'>
       <Image src='/assets/user.png' alt='Profile pic' width={44} height={44} className='h-8 w-8'/>
@@ -44,16 +61,21 @@ export function PostHeader() {
     <div className='text-[15px] flex flex-col space-y-1.5'>
       <div className='flex space-x-1.5 text-[#707e89]'>
         <span className='font-bold text-[#0f1419] whitespace-nowrap text-ellipsis overflow-hidden inline-block max-w-[60px] min[400px]:max-w-[100px] min-[500px]:max-w-[140px] sm:max-w-[160px]'>
-          Guest
+          {name}
         </span>
         <span className=' whitespace-nowrap text-ellipsis overflow-hidden inline-block max-w-[60px] min[400px]:max-w-[100px] min-[500px]:max-w-[140px] sm:max-w-[160px]'>
-          @guest1234
+          @{username}
         </span>
         <span>•</span>
-        <span>a day ago</span>
+       {
+          timestamp && 
+        <Moment fromNow>
+         {timestamp.toDate()}
+        </Moment>
+       }
       </div>
       
-      <span>hsuajidkwokdwls</span>
+      <span>{text}</span>
     </div>
     </div>
   )
--- components/PostFeed.tsx
+++ components/PostFeed.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import { Post } from './Post'
 import PostInput from './PostInput'
+import { collection, onSnapshot, orderBy, query, QueryDocumentSnapshot, DocumentData } from 'firebase/firestore'
+import { db } from '@/firebase'
 
 const PostFeed = () => {
+  const [posts, setPosts] = useState<QueryDocumentSnapshot<DocumentData, DocumentData>[]>([]);
+
+  useEffect(() => {
+  const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'))
+
+  const unsubscribe = onSnapshot(q, (snapShot) => {
+  const snapShotDocs = snapShot.docs
+
+  setPosts(snapShotDocs)
+  })
+
+  return unsubscribe
+  },[])
+
   return (
     <div className='flex-grow max-w-2xl'>
       <div className='py-4 px-3 text-lg sm:text-xl sticky top-0 z-50 bg-white bg-opacity-80 backdrop-blur-sm font-bold'>
@@ -10,7 +28,13 @@ const PostFeed = () => {
       </div>
        
        <PostInput />
-       <Post />
+
+        {posts.map(post => <Post 
+         key={post.id}
+         data={post.data()}
+        />)}
+
+       
     </div>
   )
 }
